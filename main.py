0fa37f383aa0d33e82448b059adb1054956f28d3
mauricerupp <maurice.rupp@students.unibe.ch>
2019-04-10 12:12:18 +0200

    "added u_net_convtrans_model1"

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # own classes
 import batch_generator
-import u_net_conv_upsample_model1
+import u_net_convtrans_model1
 
 # packages
 from tensorflow import keras
@@ -19,7 +19,7 @@ input_size = x_0.shape
 x_0 = None
 
 # name the model
-NAME = "u-net-Transpose"
+NAME = "u_net_convtrans_model1"
 
 # make a TensorBoard
 tensorboard = TensorBoard(log_dir='/data/cvg/maurice/logs/{}/tb_logs/'.format(NAME))
@@ -36,7 +36,7 @@ val_data_generator = batch_generator.MyGenerator(paths_dir_val + "/snaps_paths.n
                                                  paths_dir_val + "/targets_paths.npy", batchsize)
 
 # setup the model
-model = u_net_conv_upsample_model1.create_u_net_superres_model(input_size=input_size)
+model = u_net_convtrans_model1.create_u_net_superres_model(input_size=input_size)
 
 # train the model
 model.fit_generator(train_data_generator,  epochs=10, callbacks=[cp_callback, tensorboard],
