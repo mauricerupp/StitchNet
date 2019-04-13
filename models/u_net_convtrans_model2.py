34bbe3ab03078136fec448e764b2b8d996586ecd
mauricerupp <maurice.rupp@students.unibe.ch>
2019-04-13 15:36:29 +0200

    "small"

--- models/u_net_convtrans_model2.py
+++ models/u_net_convtrans_model2.py
@@ -15,20 +15,20 @@ def create_model(pretrained_weights=None, input_size=None):
     """
     inputs = Input(input_size)
     conv1 = Conv2D(64, 7, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1 = Conv2D(64, 5, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    conv1 = Conv2D(64, 7, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
     conv2 = Conv2D(128, 7, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-    conv2 = Conv2D(128, 5, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    conv2 = Conv2D(128, 7, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
     conv3 = Conv2D(256, 7, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-    conv3 = Conv2D(256, 5, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    conv3 = Conv2D(256, 7, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     conv4 = Conv2D(512, 7, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-    conv4 = Conv2D(512, 5, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    conv4 = Conv2D(512, 7, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
     drop4 = Dropout(0.5)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
 
-    conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = Conv2D(1024, 5, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
     conv5 = Conv2D(1024, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
@@ -66,3 +66,5 @@ def create_model(pretrained_weights=None, input_size=None):
         model.load_weights(pretrained_weights)
 
     return model
+
+mod = create_model(input_size=(128,128,27))
