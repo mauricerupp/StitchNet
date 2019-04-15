347d514494d9ad5b3ce4553cbd4e8addc24e8164
mauricerupp <maurice.rupp@students.unibe.ch>
2019-04-15 15:42:28 +0200

    "small"

--- models/u_net_convtrans_model3.py
+++ models/u_net_convtrans_model3.py
@@ -14,8 +14,8 @@ def create_model(pretrained_weights=None, input_size=None):
     :return:
     """
     inputs = Input(input_size)
-    conv1 = Conv2D(64, 11, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1 = Conv2D(64, 11, activation='relu', padding='same', kernel_initializer='he_normal', dilation_rate=2)(conv1)
+    conv1 = Conv2D(64, 13, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(64, 13, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
     conv2 = Conv2D(128, 11, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
     conv2 = Conv2D(128, 11, activation='relu', padding='same', kernel_initializer='he_normal', dilation_rate=2)(conv2)
