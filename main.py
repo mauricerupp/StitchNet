c187bb045e99587ce748202bd6e5ecd96faf13d5
mauricerupp <maurice.rupp@students.unibe.ch>
2019-04-28 10:33:17 +0200

    "small"

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import numpy as np
 os.environ['CUDA_VISIBLE_DEVICES'] = str(1)
 
 # set the constants
-batchsize = 79
+batchsize = 75
 paths_dir_train = '/data/cvg/maurice/processed/coco/train'
 paths_dir_val = '/data/cvg/maurice/processed/coco/val'
 x_0 = np.load(paths_dir_train + "/snaps/snaps1.npy")
