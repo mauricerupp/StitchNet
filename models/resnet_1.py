62b048f8e2590a669c9d042e7d131fd1cbdf470b
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-09 19:35:30 +0200

    "wtf"

---
+++ models/resnet_1.py
@@ -0,0 +1,46 @@
+import l1_loss
+
+from tensorflow.python.keras.models import *
+from tensorflow.python.keras.layers import *
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D
+import tensorflow as tf
+
+
+def create_model(pretrained_weights=None, input_size=None):
+    """
+    idea: process the input in packages of 3 channels at first in order to grasp the features for every picture single
+    :param pretrained_weights:
+    :param input_size:
+    :return:
+    """
+    inputs = Input(input_size)
+    # get features of every image individually and concatenate them
+    # all those layers share the same weights
+    conc1 = Lambda(feature_extractor)(inputs)
+
+    out = Conv2D(3, 3, activation='relu', padding='same', kernel_initializer='he_normal', strides=1)(conc1)
+    model = Model(inputs=inputs, outputs=out)
+    model.compile(optimizer='adam', loss=l1_loss.my_loss_l1, metrics=['accuracy'])
+    model.summary()
+
+    if pretrained_weights:
+        model.load_weights(pretrained_weights)
+
+    return model
+
+
+# ------- Functions -------- #
+def feature_extractor(input):
+    input_shape = input.get_shape().as_list()
+    layerstack = []
+    var = 0
+    for i in range(0, input_shape[3], 3):
+        conv1_x = Conv2D(64, 3, activation='relu', padding='same', name='conv1_{}'.format(var))(input[:, :, :, i:i+3])
+        layerstack.append(conv1_x)
+        var += 1
+    return concatenate([layerstack[i] for i in range(len(layerstack))], axis=3)
+
+# ------- END -------- #
+
+
+mod = create_model(input_size=(64,64,15))
