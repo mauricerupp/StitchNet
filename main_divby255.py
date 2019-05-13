43364aead731b5a72cc3f19ead7caa0a702c6483
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-13 09:21:59 +0200

    "another RDN"

--- main_divby255.py
+++ main_divby255.py
@@ -21,7 +21,7 @@ x_0 = None
 current_model = RDN_1
 
 # name the model
-NAME = str(current_model.__name__) + "_divby255"
+NAME = str(current_model.__name__) + "new_divby255"
 
 # create a TensorBoard
 tensorboard = TensorBoard(log_dir='/data/cvg/maurice/logs/{}/tb_logs/'.format(NAME))
@@ -35,43 +35,44 @@ cp_callback = keras.callbacks.ModelCheckpoint(checkpoint_path, save_weights_only
 # create a tester, that predicts the same few images after every epoch and stores them as png
 # we take 4 from the training and 4 from the validation set
 def image_predictor(epoch, logs):
-    for i in range(1,15):
-        # load X
-        if i%2 == 0:
-            x_pred = np.load('/data/cvg/maurice/processed/coco/train/snaps/snaps{}.npy'.format(i))
-        else:
-            x_pred = np.load('/data/cvg/maurice/processed/coco/val/snaps/snaps{}.npy'.format(i))
-        x_pred = np.expand_dims(x_pred, axis=0)
-        # since we train the model with values between 0 and 1 now
-        x_pred = x_pred/255.0
+    if epoch % 100 == 0:
+        for i in range(1,15):
+            # load X
+            if i%2 == 0:
+                x_pred = np.load('/data/cvg/maurice/processed/coco/train/snaps/snaps{}.npy'.format(i))
+            else:
+                x_pred = np.load('/data/cvg/maurice/processed/coco/val/snaps/snaps{}.npy'.format(i))
+            x_pred = np.expand_dims(x_pred, axis=0)
+            # since we train the model with values between 0 and 1 now
+            x_pred = x_pred/255.0
 
-        # load Y
-        if i%2 == 0:
-            y_true = np.load('/data/cvg/maurice/processed/coco/train/targets/target{}.npy'.format(i))
-        else:
-            y_true = np.load('/data/cvg/maurice/processed/coco/val/targets/target{}.npy'.format(i))
-        covered_area = y_true[:, :, -3:]
-        y_true = y_true[:, :, :-3]
-        covered_target = y_true * covered_area
+            # load Y
+            if i%2 == 0:
+                y_true = np.load('/data/cvg/maurice/processed/coco/train/targets/target{}.npy'.format(i))
+            else:
+                y_true = np.load('/data/cvg/maurice/processed/coco/val/targets/target{}.npy'.format(i))
+            covered_area = y_true[:, :, -3:]
+            y_true = y_true[:, :, :-3]
+            covered_target = y_true * covered_area
 
-        # predict y
-        y_pred = model.predict(x_pred)
-        y_pred = y_pred * 255.0
-        y_pred = np.array(np.rint(y_pred), dtype=int)
+            # predict y
+            y_pred = model.predict(x_pred)
+            y_pred = y_pred * 255.0
+            y_pred = np.array(np.rint(y_pred), dtype=int)
 
-        # save the result
-        fig = plt.figure()
-        fig.suptitle('Results of predicting Image {} on epoch {}'.format(i, epoch + 1), fontsize=20)
-        ax1 = fig.add_subplot(1, 3, 1)
-        ax1.set_title('Y_True')
-        plt.imshow(y_true[..., ::-1], interpolation='nearest')
-        ax2 = fig.add_subplot(1, 3, 2)
-        ax2.set_title('Y_True covered')
-        plt.imshow(covered_target[..., ::-1], interpolation='nearest')
-        ax3 = fig.add_subplot(1, 3, 3)
-        ax3.set_title('Prediction of model')
-        plt.imshow(y_pred[0][..., ::-1], interpolation='nearest')
-        plt.savefig("/data/cvg/maurice/logs/{}/Prediction-img{}-epoch{}.png".format(NAME, i, epoch + 1))
+            # save the result
+            fig = plt.figure()
+            fig.suptitle('Results of predicting Image {} on epoch {}'.format(i, epoch + 1), fontsize=20)
+            ax1 = fig.add_subplot(1, 3, 1)
+            ax1.set_title('Y_True')
+            plt.imshow(y_true[..., ::-1], interpolation='nearest')
+            ax2 = fig.add_subplot(1, 3, 2)
+            ax2.set_title('Y_True covered')
+            plt.imshow(covered_target[..., ::-1], interpolation='nearest')
+            ax3 = fig.add_subplot(1, 3, 3)
+            ax3.set_title('Prediction of model')
+            plt.imshow(y_pred[0][..., ::-1], interpolation='nearest')
+            plt.savefig("/data/cvg/maurice/logs/{}/Prediction-img{}-epoch{}.png".format(NAME, i, epoch + 1))
 
 
 cb_imagepredict = keras.callbacks.LambdaCallback(on_epoch_end=image_predictor)
@@ -86,7 +87,7 @@ val_data_generator = batch_generator_divby255.MyGenerator(paths_dir_val + "/snap
 model = current_model.create_model(input_size=input_size, G0=64, G=32, D=20, C=6)
 
 # train the model
-model.fit_generator(train_data_generator,  epochs=300,
-                    callbacks=[cp_callback, tensorboard],
+model.fit_generator(train_data_generator,  epochs=500,
+                    callbacks=[cp_callback, tensorboard, cb_imagepredict],
                     validation_data=val_data_generator)
 
