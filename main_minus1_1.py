c46761a660282300c8ae348fe85125f759e71ccd
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-16 18:07:27 +0200

    "bugs"

--- main_minus1_1.py
+++ main_minus1_1.py
@@ -1,6 +1,6 @@
 # own classes
 from batch_generator_minus1_1 import *
-import RDN_1
+import RDN_3
 
 # packages
 from tensorflow import keras
@@ -18,7 +18,7 @@ paths_dir_val = '/data/cvg/maurice/processed/coco_small/val'
 x_0 = np.load(paths_dir_train + "/snaps/snaps1.npy")
 input_size = x_0.shape
 x_0 = None
-current_model = RDN_1
+current_model = RDN_3
 
 # name the model
 NAME = str(current_model.__name__) + "_minus1_1"
