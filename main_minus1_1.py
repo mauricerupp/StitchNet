cb9edc6bd8a83fbc2ad9ea8d52234fab7a5c8a2a
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-17 13:05:45 +0200

    "1st version RN1"

--- main_minus1_1.py
+++ main_minus1_1.py
@@ -1,6 +1,6 @@
 # own classes
 from batch_generator_minus1_1 import *
-import RN_1_moreendlayers
+import RN_1_DTSEND
 
 # packages
 from tensorflow import keras
@@ -18,7 +18,7 @@ paths_dir_val = '/data/cvg/maurice/processed/coco_small/val'
 x_0 = np.load(paths_dir_train + "/snaps/snaps1.npy")
 input_size = x_0.shape
 x_0 = None
-current_model = RN_1_moreendlayers
+current_model = RN_1_DTSEND
 
 # name the model
 NAME = str(current_model.__name__) + "_minus1_1"
