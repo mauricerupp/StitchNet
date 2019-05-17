c207de971bd800ea88d12427a41d914a10d6bf32
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-17 12:34:08 +0200

    "1st version RN1"

--- main_minus1_1.py
+++ main_minus1_1.py
@@ -1,6 +1,6 @@
 # own classes
 from batch_generator_minus1_1 import *
-import RN_1_globalskips
+import RN_1
 
 # packages
 from tensorflow import keras
@@ -18,10 +18,10 @@ paths_dir_val = '/data/cvg/maurice/processed/coco_small/val'
 x_0 = np.load(paths_dir_train + "/snaps/snaps1.npy")
 input_size = x_0.shape
 x_0 = None
-current_model = RN_1_globalskips
+current_model = RN_1
 
 # name the model
-NAME = str(current_model.__name__) + "2_minus1_1"
+NAME = str(current_model.__name__) + "_minus1_1"
 
 
 # ----- Callbacks / Helperfunctions ----- #
