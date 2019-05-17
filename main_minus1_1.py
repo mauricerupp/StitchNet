d3104863677bd03c4d1f9d3722ce7a6e55de211a
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-17 12:50:15 +0200

    "1st version RN1"

--- main_minus1_1.py
+++ main_minus1_1.py
@@ -21,7 +21,7 @@ x_0 = None
 current_model = RN_1
 
 # name the model
-NAME = str(current_model.__name__) + "_minus1_1"
+NAME = str(current_model.__name__) + "bigfirstfilter_minus1_1"
 
 
 # ----- Callbacks / Helperfunctions ----- #
