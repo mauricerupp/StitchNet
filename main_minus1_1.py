8f37468c41278ac6b46b7dd79da0d5f68c4ac8ec
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-22 13:49:03 +0200

    "implemented RN2"

--- main_minus1_1.py
+++ main_minus1_1.py
@@ -1,6 +1,6 @@
 # own classes
 from batch_generator_minus1_1 import *
-import RN_1
+import RN_2
 
 # packages
 from tensorflow import keras
@@ -18,10 +18,10 @@ paths_dir_val = '/data/cvg/maurice/processed/coco_small/val'
 x_0 = np.load(paths_dir_train + "/snaps/snaps1.npy")
 input_size = x_0.shape
 x_0 = None
-current_model = RN_1
+current_model = RN_2
 
 # name the model
-NAME = str(current_model.__name__) + "_moreRB_BATCH"
+NAME = str(current_model.__name__) + "_BATCH"
 
 
 # ----- Callbacks / Helperfunctions ----- #
@@ -88,7 +88,7 @@ train_data_generator = MyGenerator(paths_dir_train + "/snaps_paths.npy", paths_d
 val_data_generator = MyGenerator(paths_dir_val + "/snaps_paths.npy", paths_dir_val + "/targets_paths.npy", batchsize)
 
 # ----- Model setup ----- #
-model = current_model.create_model(input_size=input_size, filter_size=128, block_amount=20, normalizer="batch")
+model = current_model.create_model(input_size=input_size, filter_size=128, block_amount=12, normalizer="batch")
 
 # train the model
 model.fit_generator(train_data_generator,  epochs=202,
---
+++ models/RN/RN_2.py
@@ -0,0 +1,109 @@
+import l1_loss
+from group_normalization import InstanceNormalization
+from RDN_1 import feature_extract
+
+
+from tensorflow.python.keras.models import *
+from tensorflow.python.keras.layers import *
+from tensorflow.python.keras.utils import plot_model
+import tensorflow as tf
+import datetime
+
+
+def create_model(pretrained_weights=None, input_size=None, filter_size=128, block_amount = 12, normalizer=None):
+    """
+    A simple residual network with ResBlocks from Givi and added the feature extraction layer of RDNs in the beginning
+    :param G0: filtersize for the last convolutional layer
+    :param G: filtersize per convolutional layer
+    :param D: amout of residual dense blocks (RDB)
+    :param C: Amount of convolutional layers per RDB
+    :param pretrained_weights:
+    :param input_size:
+    :return:
+    """
+    inputs = Input(input_size)
+
+    # feature extractor of RDNs
+    conv1 = feature_extract(inputs, 64)
+    # first global conv has no normalization
+    global_conv1 = Conv2D(filter_size*2, kernel_size=7, activation='relu', padding='same', name='global_conv1')(conv1)
+
+    global_conv2 = Conv2D(filter_size, kernel_size=7, padding='same', name='global_conv2')(global_conv1)
+    global_conv2 = norm(name="globalconv2_norm1", input_layer=global_conv2, normalizer=normalizer)
+    global_conv2 = Activation('relu')(global_conv2)
+
+    # first RB
+    RB = create_resblock(prior_layer=global_conv2, block_name='RB1',
+                         n_filters=filter_size, kernel_size=3, stride=1, dilation=1, normalizer=normalizer)
+
+    # add the remaining RDB
+    for i in range(2, block_amount + 1):
+        RB = create_resblock(prior_layer=RB, block_name='RB' + str(i),
+                             n_filters=filter_size, kernel_size=3, stride=1, dilation=1, normalizer=normalizer)
+
+    # depth to space
+    out = depth_to_space(RB, 2)
+
+    # TODO: Add layers here?
+    out = Conv2D(16, kernel_size=5, padding='same', activation='relu')(out)
+    out = Conv2D(8, kernel_size=5, padding='same', activation='relu')(out)
+
+    # since we output a color image, we want 3 filters as the last layer
+    out = Conv2D(3, kernel_size=5, padding='same', activation='tanh')(out)
+
+    model = Model(inputs=inputs, outputs=out)
+    model.compile(optimizer='adam', loss=l1_loss.custom_loss, metrics=['accuracy'])
+    model.summary()
+
+    # Save the configurations as txt-file
+    #with open('RDN ' + str(datetime.datetime.now()) + ' config.txt', 'w') as fh:
+    #    model.summary(print_fn=lambda x: fh.write(x + '\n'))
+
+    #plot_model(model, to_file='RN_1.png')
+
+    if pretrained_weights:
+        model.load_weights(pretrained_weights)
+
+    return model
+
+
+# ------- Functions -------- #
+def create_resblock(prior_layer, block_name, n_filters, kernel_size, stride, dilation, normalizer):
+    x = Conv2D(filters=n_filters, kernel_size=kernel_size,
+               strides=stride, dilation_rate=dilation,
+               name=block_name + "_conv1",
+               padding='same')(prior_layer)
+    x = norm(x, name=block_name + "_norm1", normalizer=normalizer)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters=n_filters, kernel_size=kernel_size,
+               strides=stride, dilation_rate=dilation,
+               name=block_name + "_conv2",
+               padding='same')(x)
+
+    x = norm(x, name=block_name + "_norm2", normalizer=normalizer)
+
+    return Add()([prior_layer, x])
+
+
+def depth_to_space(input_layer, blocksize):
+    """
+    implements the tensorflow depth to space function
+    :param input_layer:
+    :return:
+    """
+    return Lambda(lambda x: tf.depth_to_space(x, block_size=blocksize, data_format='NHWC'), name='Depth_to_Space',)(input_layer)
+
+
+def norm(input_layer, name, normalizer):
+    if normalizer.lower() == 'batch':
+        return BatchNormalization(name=name)(input_layer)
+    elif normalizer.lower() == 'instance':
+        return InstanceNormalization()(input_layer)
+    else:
+        print("No valid norm")
+        exit()
+# ------- END -------- #
+
+
+mod = create_model(input_size=(64,64,15), filter_size=128, block_amount=10, normalizer='batch')
