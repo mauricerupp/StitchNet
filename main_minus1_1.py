d8adfd3f630b07abd61323b3065dd1825d54722f
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-29 15:31:21 +0200

    "implemented mean to l1-loss"

--- main_minus1_1.py
+++ main_minus1_1.py
@@ -21,7 +21,7 @@ x_0 = None
 current_model = RN_1
 
 # name the model
-NAME = str(current_model.__name__) + "_new_l1"
+NAME = str(current_model.__name__) + ""
 
 
 # ----- Callbacks / Helperfunctions ----- #
