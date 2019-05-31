942057d29b4b5d1738cc7f4fd0dd9407ce7a257e
mauricerupp <maurice.rupp@students.unibe.ch>
2019-05-31 20:50:05 +0200

    "implemented autoencoder"

--- autoencoder/pathgenerator.py
+++ autoencoder/pathgenerator.py
@@ -4,7 +4,7 @@ import os
 dir = '/data/cvg/maurice/unprocessed/coco_smalltrain/'
 snaps_paths = []
 storage_dir = '/data/cvg/maurice/unprocessed/'
-for img in os.listdir():
+for img in os.listdir(dir):
     snaps_paths.append(os.path.join(dir, img))
 
 np.save(storage_dir + "train_snaps_paths.npy", snaps_paths)
