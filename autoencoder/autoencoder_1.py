8dab0e3d5c9bddff9729f7e138779e81448b8e70
mauricerupp <maurice.rupp@students.unibe.ch>
2019-06-01 13:43:02 +0200

    "implemented autoencoder"

--- autoencoder/autoencoder_1.py
+++ autoencoder/autoencoder_1.py
@@ -19,7 +19,7 @@ class ConvAutoencoder(object):
         self.encoder = Model(inputs, encoder_layers, name='encoder')
         self.decoder = Model(decoder_inputs, decoder_layers, name='decoder')
 
-        self.autoencoder = Model(inputs, self.decoder(self.encoder.output), name='autoencoder')
+        self.autoencoder = Model(self.encoder.inputs, self.decoder(self.encoder.output), name='autoencoder')
         self.autoencoder.summary()
         self.autoencoder.compile(optimizer='adam', loss='mean_absolute_error', metrics=['accuracy'])
 
@@ -27,4 +27,4 @@ class ConvAutoencoder(object):
         self.encoder.load_weights(filepath=path)
 
 
-#mod = ConvAutoencoder(input_size=(64,64,3))
+mod = ConvAutoencoder(input_size=(64,64,3))
