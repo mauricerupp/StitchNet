df1d8fb222a5eec2e9286a11ea0c8d79a0895378
mauricerupp <maurice.rupp@students.unibe.ch>
2019-06-01 13:59:50 +0200

    "implemented autoencoder"

--- autoencoder/autoencoder_1.py
+++ autoencoder/autoencoder_1.py
@@ -11,12 +11,24 @@ class ConvAutoencoder(object):
     def __init__(self, input_size):
         # setup to encoding/decoding
         inputs = Input(input_size, name='encoder_input')
-        encoder_layers = encode(inputs)
-        decoder_layers = decode(encoder_layers)
+
+        conv = Conv2D(64, 3, activation='relu', padding='same', name='encoder_conv1')(inputs)
+        conv = Conv2D(128, 3, activation='relu', padding='same', name='encoder_conv2', strides=2)(conv)
+        conv = Conv2D(256, 3, activation='relu', padding='same', name='encoder_conv3', strides=2)(conv)
+        conv = Conv2D(256, 3, activation='relu', padding='same', name='encoder_conv4', strides=2)(conv)
+        conv = Conv2D(256, 3, activation='relu', padding='same', name='encoder_conv5', strides=2)(conv)
+
+        deconv = Conv2DTranspose(128, 3, activation='relu', padding='same', name='decoder_conv1', strides=2)(conv)
+        deconv = Conv2DTranspose(64, 3, activation='relu', padding='same', name='decoder_conv2', strides=2)(deconv)
+        deconv = Conv2DTranspose(32, 3, activation='relu', padding='same', name='decoder_conv3', strides=2)(deconv)
+        deconv = Conv2DTranspose(16, 3, activation='relu', padding='same', name='decoder_conv4', strides=2)(deconv)
+        deconv =  Conv2DTranspose(3, 3, activation='tanh', padding='same', name='decoder_conv5', strides=1)(deconv)
+        #encoder_layers = encode(inputs)
+        #decoder_layers = decode(encoder_layers)
 
         #self.encoder = Model(inputs, encoder_layers, name='encoder')
 
-        self.autoencoder = Model(inputs, decoder_layers, name='autoencoder')
+        self.autoencoder = Model(inputs, deconv, name='autoencoder')
         self.autoencoder.summary()
         self.autoencoder.compile(optimizer='adam', loss='mean_absolute_error', metrics=['accuracy'])
 
