c1c762a8133f2a0ce00cf4e7f94ae10ccbc2ba04
mauricerupp <maurice.rupp@students.unibe.ch>
2019-06-23 11:22:36 +0200

    "autoencoder v4"

--- main.py
+++ main.py
@@ -98,5 +98,5 @@ model = StitchDecoder(input_size, '/data/cvg/maurice/logs/ConvAutoencoder_V4_bat
 # train the model
 model.stitchdecoder.fit_generator(train_data_generator,  epochs=2002,
                     callbacks=[cp_callback, tensorboard, cb_imagepredict],
-                    validation_data=val_data_generator, max_queue_size=15, workers=10)
+                    validation_data=val_data_generator, max_queue_size=32, workers=16)
 
