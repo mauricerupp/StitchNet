aca3c2650515fba1e42da76560a56ac843292180
mauricerupp <maurice.rupp@students.unibe.ch>
2019-07-09 15:32:05 +0200

    "randompaths"

--- losses/perceptual_loss.py
+++ losses/perceptual_loss.py
@@ -19,7 +19,9 @@ def vgg_loss(y_true, y_pred):
 
     # MAE has shape of (64x64), Perceptual has shape of (8x8), therefore we take the mean of those values and
     # add a scale factor, so they have the same general scale, so they can be weighted properly
-    return percentage_MAE * K.mean(mean_absolute_error(y_true, y_pred)) + SCALE * percentage_perceptual * perceptual_loss(y_true, y_pred)
+    return percentage_MAE * K.mean(mean_absolute_error(y_true, y_pred)) + \
+           SCALE * percentage_perceptual * perceptual_loss(preprocess_input(revert_zero_center(y_true)*255.0),
+                                                           preprocess_input(revert_zero_center(y_pred)*255.0))
 
 
 def perceptual_loss(y_true, y_pred):
@@ -37,7 +39,7 @@ def perceptual_loss(y_true, y_pred):
     model = Model(inputs=my_vgg16.input, outputs=my_vgg16.get_layer('block4_conv3').output)
     model.trainable = False
     # preprocess input works with data in the range of [0,255], so the images have to be reverted
-    #yt_new = tf.keras.applications.vgg16.preprocess_input(revert_zero_center(y_true)*255.0)
-    #yp_new = tf.keras.applications.vgg16.preprocess_input(revert_zero_center(y_pred)*255.0)
+    yt_new = preprocess_input(revert_zero_center(y_true)*255.0)
+    yp_new = preprocess_input(revert_zero_center(y_pred)*255.0)
     # since we here have 8x8=64 pixels, we have to scale the result
     return K.mean(mean_squared_error(model(y_true), model(y_pred)))
