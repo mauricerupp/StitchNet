fdac6e666e26429f6c7201125582976925340e49
mauricerupp <maurice.rupp@students.unibe.ch>
2019-07-14 16:14:54 +0200

    "randompaths"

--- stitch_decoder_v1.py
+++ stitch_decoder_v1.py
@@ -38,19 +38,20 @@ class StitchDecoder(object):
         self.stitchdecoder = Model(inputs=encoder_inputs, outputs=out, name='stitcher')
         self.stitchdecoder.summary()
         # enable multi-gpu-processing
-        self.stitchdecoder = multi_gpu_model(self.stitchdecoder, gpus=2)
+        #self.stitchdecoder = multi_gpu_model(self.stitchdecoder, gpus=2)
         self.stitchdecoder.compile(optimizer='adam', loss=custom_loss, metrics=['accuracy', stitched_PSNR])
 
     def load_weights(self, path):
         self.stitchdecoder.load_weights(filepath=path)
 
-"""
+
 mod = StitchDecoder(input_size=(64, 64, 15),
                     encoderweights_path='/data/cvg/maurice/logs/ConvAutoencoder_V5fixed_instanceBIGGER_20_80_run3/encoder_logs/',
                     normalizer='instance',
                     isTraining=True)
 """
 mod = StitchDecoder(input_size=(64, 64, 15),
-                    encoderweights_path='/data/cvg/maurice/logs/ConvAutoencoder_V5fixed_instanceBIGGER_20_80_run3/encoder_logs/',
+                    encoderweights_path='/home/maurice/Dokumente/encoder_logs/',
                     normalizer='instance',
                     isTraining=True)
+"""
