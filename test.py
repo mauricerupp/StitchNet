5d3897936eb77505f54538832f2bfb82c22d2de5
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-07 12:34:07 +0200

    "augmenting dataset"

--- test.py
+++ test.py
@@ -19,6 +19,10 @@ from tensorflow.python.keras.utils import plot_model
 import tensorflow as tf
 import datetime
 
+data = np.array('/home/maurice/Dokumente/Try_Models/coco_try/TR/000000504554.jpg',
+                )
+train_data_generator = MyGenerator(np.arra[data, 1)
+
 matrix = np.random.choice(a=[0, 1], size=(10, 5), p=[1, 0])
 print(matrix)
 """
