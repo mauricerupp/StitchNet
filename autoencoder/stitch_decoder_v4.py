9c1b23186c238fd5e6e4e4c0064d8f261a33ba76
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-22 08:51:07 +0200

    "augmenting dataset"

--- autoencoder/stitch_decoder_v4.py
+++ autoencoder/stitch_decoder_v4.py
@@ -19,7 +19,7 @@ class StitchDecoder(object):
         encoder_inputs = Input(shape=input_size)
 
         autoenc = ConvAutoencoder([input_size[0], input_size[1], 3], norm=normalizer, isTraining=False)
-        #autoenc.load_encoder_weights(encoderweights_path)
+        autoenc.load_encoder_weights(encoderweights_path)
         enc = autoenc.encoder
 
         # encode each image individually through the pre-trained encoder
