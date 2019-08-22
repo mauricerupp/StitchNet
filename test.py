c8401304bcf23e089da5d68c4202ecc5a6447833
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-22 19:44:55 +0200

    "augmenting dataset"

--- test.py
+++ test.py
@@ -68,7 +68,7 @@ y_true = np.array(zero_center(y_true/255.0), dtype=np.float32)
 autoenc = ConvAutoencoder(input_size, norm='instance', isTraining=False)
 print([layer.name for layer in autoenc.autoencoder.layers])
 
-weights = autoenc.autoencoder.get_layer(name='bottleneck_relu_layer').get_weights()
+weights = autoenc.autoencoder.get_layer('autoencoder').get_layer(name='bottleneck_relu_layer').get_weights()
 weights = np.array(weights)
 print(weights[0][0][0])
 print("_______________________")
@@ -77,7 +77,7 @@ print("_______________________")
 autoenc.autoencoder.load_weights('/data/cvg/maurice/logs/ConvAutoencoder_V6_instance_20_80_newcallback/weight_logs/auto_weights-improvement-01.h5')
 y_pred = autoenc.autoencoder.predict(y_true)
 
-weights = autoenc.autoencoder.get_layer(name='bottleneck_relu_layer').get_weights()
+weights = autoenc.autoencoder.get_layer('autoencoder').get_layer(name='bottleneck_relu_layer').get_weights()
 weights = np.array(weights)
 print(weights[0][0][0])
 
