3f4eac6429685bdffdecb41a9fe46b40614bb68b
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-28 15:38:47 +0200

    "augmenting dataset"

--- autoencoder/stitch_decoder_v4.py
+++ autoencoder/stitch_decoder_v4.py
@@ -39,7 +39,9 @@ class StitchDecoder(object):
             encoded_img_list.append(encoder_model(x))
 
             debug = revert_zero_center(autoenc.autoencoder(x)) * 255
-            debug = debug.eval()
+            sess = tf.Session()
+            with sess.as_default():
+                debug = debug.eval()
             y_pred = np.array(np.rint(debug), dtype=int)
             fig = plt.figure()
             fig.suptitle('Results of predicting {}Image {}\n on epoch {}'.format(set, i, epoch + 1), fontsize=20)
