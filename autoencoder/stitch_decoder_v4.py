784f286328cbe2628f4e93cf5ede3ebbd97537b5
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-28 16:01:55 +0200

    "augmenting dataset"

--- autoencoder/stitch_decoder_v4.py
+++ autoencoder/stitch_decoder_v4.py
@@ -38,13 +38,14 @@ class StitchDecoder(object):
             x = Lambda(lambda x: x[:, :, :, i:i + 3], name='img_{}'.format(str(index)))(encoder_inputs)
             encoded_img_list.append(encoder_model(x))
 
+            """
             debug = revert_zero_center(autoenc.autoencoder(x)) * 255
             sess = tf.Session()
-            sinogram = tf.cast(debug[0], tf.uint8)
-            sinogram = tf.image.encode_jpeg(sinogram, quality=100)
-            writer = tf.write_file("/data/cvg/maurice/logs/{}/Prediction-img{}-{}.jpeg".format(debug, i, time.time()), sinogram)
+            autoenc_img = tf.cast(debug[0], tf.uint8)
+            autoenc_img = tf.image.encode_jpeg(autoenc_img, quality=100)
+            writer = tf.write_file("/data/cvg/maurice/logs/{}/Prediction-img{}-{}.jpeg".format(debug, i, time.time()), autoenc_img)
             sess.run(writer)
-            """
+           
             y_pred = np.array(np.rint(debug), dtype=int)
             fig = plt.figure()
             fig.suptitle('Results of predicting {}Image {}\n on epoch {}'.format(set, i, epoch + 1), fontsize=20)
