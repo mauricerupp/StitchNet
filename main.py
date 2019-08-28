5ff33d0c892e5a99ebc37ec7d7182ac357e0b1c9
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-28 20:21:50 +0200

    "augmenting dataset"

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ input_size = [64,64,15]
 current_model = StitchDecoder
 
 # name the model
-NAME = str(current_model.__name__) + "_S2_20_80_140samples"
+NAME = str(current_model.__name__) + "_S2_20_80_DEBUG"
 
 
 # ----- Callbacks / Helperfunctions ----- #
