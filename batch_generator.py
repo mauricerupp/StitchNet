44d1e3e28bc034938ffa4fabbfb4957bbf60f557
mauricerupp <maurice.rupp@students.unibe.ch>
2019-08-30 21:19:23 +0200

    "augmenting dataset"

--- batch_generator.py
+++ batch_generator.py
@@ -9,9 +9,10 @@ from very_random_path_one_img import create_very_rand_path
 
 class MyGenerator(Sequence):
 
-    def __init__(self, raw_paths, batch_size):
+    def __init__(self, raw_paths, batch_size, dataset="S2"):
         self.snaps = np.load(raw_paths)
         self.batch_size = batch_size
+        self.dataset = dataset
 
     def __len__(self):
         return math.ceil(len(self.snaps) / float(self.batch_size))
@@ -21,7 +22,12 @@ class MyGenerator(Sequence):
         inputs = []
         targets = []
         for img_path in batch:
-            output = create_smooth_rand_path(img_path)
+            if self.dataset == "S1":
+                output = create_fixed_path(img_path)
+            elif self.dataset == "S2":
+                output = create_smooth_rand_path(img_path)
+            else:
+                output = create_very_rand_path(img_path)
 
             inputs.append(output[0])
             targets.append(output[1])
