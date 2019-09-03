5cb86a4e519ffc478925b62b0d7c80722aa41ca2
mauricerupp <maurice.rupp@students.unibe.ch>
2019-09-03 21:42:39 +0200

    "augmenting dataset"

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ input_size = [64,64,15]
 current_model = StitchDecoder
 
 # name the model
-NAME = str(current_model.__name__) + "_S2_100_0_DEBUG_10samples_BATCH"
+NAME = str(current_model.__name__) + "TESTEEE"
 
 
 # ----- Callbacks / Helperfunctions ----- #
@@ -35,10 +35,10 @@ def image_predictor(epoch, logs):
             # load X
             set = ""
             if i % 2 == 0:
-                list = np.load('/data/cvg/maurice/unprocessed/smallval_snaps_paths.npy')
+                list = np.load('/data/cvg/maurice/unprocessed/train_snaps_paths.npy')
                 set += "train-"
             else:
-                list = np.load('/data/cvg/maurice/unprocessed/smallval_snaps_paths.npy')
+                list = np.load('/data/cvg/maurice/unprocessed/val_snaps_paths.npy')
                 set += "test-"
 
             # create a random path
@@ -84,12 +84,12 @@ filepath = SAVE_PATH + '_weights-improvement-{epoch:02d}.hdf5'
 cp_callback = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=False, mode='max', period=400, save_weights_only=True)
 
 # ----- Batch-generator setup ----- #
-train_data_generator = MyGenerator(paths_dir + "smallval_snaps_paths.npy", batchsize)
-val_data_generator = MyGenerator(paths_dir + "smallval_snaps_paths.npy", batchsize)
+train_data_generator = MyGenerator(paths_dir + "train_snaps_paths.npy", batchsize)
+val_data_generator = MyGenerator(paths_dir + "val_snaps_paths.npy", batchsize)
 
 # ----- Model setup ----- #
 model = StitchDecoder(input_size, '/data/cvg/maurice/logs/ConvAutoencoder_V6_instance_20_80_newcallback_run4/weight_logs/auto_weights-improvement-133.hdf5',
-                      normalizer='batch', isTraining=True)
+                      normalizer='instance', isTraining=True)
 #model.load_weights('/data/cvg/maurice/logs/StitchDecoder_AEv6_D2v4_MAE/weight_logs/')
 
 # train the model
