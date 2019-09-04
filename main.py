155812308e1139215c100ef3cc4552d04024bd2c
mauricerupp <maurice.rupp@students.unibe.ch>
2019-09-04 10:34:21 +0200

    "augmenting dataset"

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 tf.keras.backend.clear_session()
 
 # set the constants
-batchsize = 64
+batchsize = 32
 paths_dir = '/data/cvg/maurice/unprocessed/'
 input_size = [64,64,15]
 current_model = StitchDecoder
