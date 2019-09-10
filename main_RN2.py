ba8fa51682564cfb47dbf6149cc5c0943504225d
mauricerupp <maurice.rupp@students.unibe.ch>
2019-09-10 18:07:30 +0200

    "augmenting dataset"

--- main_RN2.py
+++ main_RN2.py
@@ -62,8 +62,9 @@ def image_predictor(epoch, logs):
             y_true = y_true[:, :, :-3]
             y_true = revert_zero_center(y_true) * 255
             y_true = np.array(np.rint(y_true), dtype=int)
-
+            print(covered_area)
             covered_target = y_true * covered_area
+            print(covered_target)
 
             # predict y (since the model is trained on pictures in [-1,1])
             y_pred = model.predict(x)
