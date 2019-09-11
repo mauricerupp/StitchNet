7ad5d87cb77f6c4f9fa87e228b88ff725ef7f12c
mauricerupp <maurice.rupp@students.unibe.ch>
2019-09-11 22:48:00 +0200

    "augmenting dataset"

--- models/RDN/RDN_3.py
+++ models/RDN/RDN_3.py
@@ -77,4 +77,4 @@ def create_model(pretrained_weights=None, input_size=None, G0=64, G=32, D=20, C=
 
     return model
 
-mod = create_model(input_size=(64,64,15), D=10, C=6, G0=320, G=64)
+#mod = create_model(input_size=(64,64,15), D=10, C=6, G0=320, G=64)
