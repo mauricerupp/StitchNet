e972c202c18850ca7dc4bbed582ed08dd151c23d
mauricerupp <maurice.rupp@students.unibe.ch>
2019-09-13 13:58:42 +0200

    "augmenting dataset"

--- main_RDN.py
+++ main_RDN.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 tf.keras.backend.clear_session()
 
 # set the constants
-batchsize = 16
+batchsize = 24
 paths_dir = '/data/cvg/maurice/unprocessed/'
 input_size = [64,64,15]
 
