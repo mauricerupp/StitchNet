54313197adcb79d6e769ba6909f633814f0bbe09
mauricerupp <maurice.rupp@students.unibe.ch>
2019-09-15 08:41:59 +0200

    "augmenting dataset"

--- main_RDN.py
+++ main_RDN.py
@@ -21,7 +21,7 @@ paths_dir = '/data/cvg/maurice/unprocessed/'
 input_size = [64,64,15]
 
 # name the model
-DATASET = "S2"
+DATASET = "S3"
 NAME = "RDN_" + DATASET
 
 
@@ -101,7 +101,7 @@ val_data_generator = MyGenerator(paths_dir + "val_snaps_paths.npy", batchsize, D
 
 # ----- Model setup ----- #
 model = create_model(input_size=input_size, D=10, C=6, G0=320, G=32)
-model.load_weights('/data/cvg/maurice/logs/RDN_3_minus1_1/weight_logs/')
+#model.load_weights('/data/cvg/maurice/logs/RDN_3_minus1_1/weight_logs/')
 
 # train the model
 model.fit_generator(train_data_generator,  epochs=702,
